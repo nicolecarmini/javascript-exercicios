e62da984a509f2db877eb369ac988c92a7f6007d
João Vitor Lima <joaolimacaldeira.ti@gmail.com>
2024-10-14 17:59:44 -0300

    update variaveis

---
+++ variaveis/script.js
@@ -0,0 +1,28 @@
+// isso é um comentário
+
+// tipos primitivos 
+
+//number 
+let pi= 3.1415
+console.log(` A variável  ${pi} é do tipo ${typeof(pi)}`)
+
+// string
+let nome = "Pafuncio"
+console.log(`A variável ${nome} é do tipo string`)
+
+//boolean
+let corinthiansrebaixado = true
+console.log(` A variável  ${corinthiansrebaixado} é do tipo ${typeof(corinthiansrebaixado)}`)
+
+// macaco da barriga gorda 
+let frutas = ["pera, uva, maçã, manga"]
+console.log(` A variável  ${frutas} é do tipo ${typeof(frutas)}`)
+
+function chablau(){
+    console.log("Oi sou a funçao chablau")
+};
+
+chablau()
+
+ let vamoquevamo = function(){}
+ console.log(` A variável  ${vamoquevamo} é do tipo ${typeof(vamoquevamo)}`)
