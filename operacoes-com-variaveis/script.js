8efb72c79902cfd9033c3c2a6a260509930ae3d7
João Vitor Lima <joaolimacaldeira.ti@gmail.com>
2024-10-14 18:45:24 -0300

    operacoes-com-variaveis

---
+++ operacoes-com-variaveis/script.js
@@ -0,0 +1,53 @@
+// operções com variáveis
+
+//operadores matematicos
+
+let comparacao = "0" == 0;
+let soma = 3+3
+console.log(soma)
+
+let subtrair = 7-4
+console.log(subtrair)
+
+let multiplicar = 6*4
+console.log(multiplicar)
+
+let dividir = 10/5
+console.log(dividir)
+
+let divisaointeira = 5%2
+console.log(divisaointeira)
+
+//operadores de comparação
+
+console.log(comparacao)
+
+let comparacao2 = "1" === 1;
+console.log(comparacao2)
+
+
+let maiorque = 5>2
+console.log(maiorque)
+
+let menorque = 3<4
+console.log(menorque)
+
+let maiorouigual = 4<=8
+console.log(maiorouigual)
+
+let menorouigual = 4<=4
+console.log(menorouigual)
+
+//operadores lógicos
+let numero1 = 3
+let numero2 = 4
+
+let comparacao3 = numero1 == 3 && numero2 == 2
+console.log(comparacao3)
+
+
+let comparacao4 = numero1 == 3 || numero2 == 2
+console.log(comparacao4)
+
+
+console.log(!comparacao3)
