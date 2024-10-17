54da43228bac519c0bc111e4f298377da5c63a0d
nicolecarmini <nicolecarminimarin76@gmail.com>
2024-10-17 17:49:47 -0300

    add variaveis folder

---
+++ desafio-1-variaveis/desafio1.js
@@ -0,0 +1,8 @@
+let valorCombustivel = 5.7;
+let consumo = 10;
+let distancia = 60;
+let litrosConsumidos = (distancia/litrosConsumidos)
+
+let valorTotal= (valorCombustivel*litrosConsumidos)
+
+console.log(valorTotal)
